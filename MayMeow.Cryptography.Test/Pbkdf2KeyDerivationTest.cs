2ca518cabcf974b7530faef718b8175b0cc9eed8
MayMeow <3164256+MayMeow@users.noreply.github.com>
2021-09-16 08:28:56 +0200

    Add PBKDF2 key derivation example

---
+++ MayMeow.Cryptography.Test/Pbkdf2KeyDerivationTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MayMeow.Cryptography;
+
+namespace MayMeow.Cryptography.Test
+{
+    [TestClass]
+    public class Pbkdf2KeyDerivationTest
+    {
+        [TestMethod]
+        public void KeyDerivation()
+        {
+            // Set variables
+            string password = "my$up3r$3cr3tP4$$w0rd1";
+            string salt = "8VySCxa42j9McSqGjZxCVQnH4x4rSZszEL9YQT3VkZ75xbBD";
+            string requestedKey = "Vu+/ve+/ve+/vWfvv71d77+9Ou+/vQ==";
+
+            // get your derived key
+            var derivedKey = PBKDF2.keyDerivate(password, salt, 1024, 10);
+
+            // I converting this to base64 to compare if keys are equal.
+            Assert.AreEqual(requestedKey, TextConversion.Base64Encode(derivedKey));
+        }
+    }
+}
---
+++ MayMeow.Cryptography/PBKDF2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+namespace MayMeow.Cryptography
+{
+    public class PBKDF2
+    {
+        /// <summary>
+        ///  PBKDF2 can be used to derivate cryptography key from password.
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="salt"></param>
+        /// <param name="iterationCount"></param>
+        /// <param name="keyLenghtBytes"></param>
+        /// <returns></returns>
+        public static string keyDerivate(string password, string salt, int iterationCount = 1024, int keyLenghtBytes = 256)
+        {
+            var key = KeyDerivation.Pbkdf2(password, System.Text.Encoding.UTF8.GetBytes(salt), KeyDerivationPrf.HMACSHA256, iterationCount, keyLenghtBytes);
+
+            return System.Text.Encoding.UTF8.GetString(key);
+        }
+    }
+}
