aeecc1bdcb2cbce1f90adda048faa47cefa30cd4
May Meow <may@maymeow.com>
2024-02-03 20:18:35 +0100

    Added ECDSA example

---
+++ MayMeow.Cryptography.Test/EcdsaTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayMeow.Cryptography.Test
+{
+    [TestClass]
+    public class EcdsaTest
+    {
+        [TestMethod]
+        public void SigningTest()
+        {
+            ECDSA eCDSA = new ECDSA();
+            ECParameters ecParameters = eCDSA.getKeys();
+
+            string dataToSign = "Hello World!";
+
+            byte[] signature = ECDSA.SignBytes(Encoding.UTF8.GetBytes(dataToSign), ecParameters);
+
+            bool isVerified = ECDSA.VerifyBytes(Encoding.UTF8.GetBytes(dataToSign), signature, ecParameters);
+
+            Assert.IsTrue(isVerified);
+        }
+    }
+}
---
+++ MayMeow.Cryptography/ECDSA.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MayMeow.Cryptography
+{
+    public class ECDSA
+    {
+
+        protected ECDsa eCDsa;
+
+        protected ECParameters ecParameters;
+
+        public ECDSA()
+        {
+            using (eCDsa = ECDsa.Create())
+            {
+                eCDsa.GenerateKey(ECCurve.NamedCurves.nistP256);
+                ecParameters = eCDsa.ExportParameters(true);
+            }
+        }
+
+        public string getPrivateKey()
+        {
+            return createXML(eCDsa.ExportParameters(true));
+        }
+
+        public string getPublicKey()
+        {
+            return createXML(eCDsa.ExportParameters(false));
+        }
+
+        private string createXML<T>(T parameters)
+        {
+            StringWriter sw = new StringWriter();
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            xs.Serialize(sw, parameters);
+
+            return sw.ToString();
+        }
+
+        /// <summary>
+        /// Return public and private key parameters
+        /// </summary>
+        /// <returns></returns>
+        public ECParameters getKeys()
+        {
+            return ecParameters;
+        }
+
+        /// <summary>
+        /// Sign data
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static byte[] SignBytes(byte[] data , ECParameters parameters)
+        {
+            ECDsa ecvsaSigner = ECDsa.Create();
+            byte[] signature;
+
+            using (ecvsaSigner)
+            {
+                ecvsaSigner.ImportParameters(parameters);
+                signature = ecvsaSigner.SignData(data, HashAlgorithmName.SHA256);
+                
+            }
+
+            return signature;
+        }
+
+        /// <summary>
+        /// Verify signed data
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="signature"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static bool VerifyBytes(byte[] data, byte[] signature, ECParameters parameters)
+        {
+            ECDsa ecvsaSigner = ECDsa.Create();
+            bool isVerified;
+
+            using (ecvsaSigner)
+            {
+                ecvsaSigner.ImportParameters(parameters);
+                isVerified = ecvsaSigner.VerifyData(data, signature, HashAlgorithmName.SHA256);
+
+            }
+
+            return isVerified;
+        }
+
+    }
+
+}
